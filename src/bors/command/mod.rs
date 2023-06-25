d5f92b84c9873f1f68a1d521ce982e920942e0d9
Jakub Beránek <berykubik@gmail.com>
2023-06-25 16:26:33 +0200

    Parse key-value arguments in bors commands

--- src/bors/command/mod.rs
+++ src/bors/command/mod.rs
@@ -1,13 +1,17 @@
 mod parser;
+use crate::github::CommitSha;
 pub use parser::{CommandParseError, CommandParser};
 
 /// Bors command specified by a user.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum BorsCommand {
     /// Ping the bot.
     Ping,
     /// Perform a try build.
-    Try,
+    Try {
+        /// Parent commit which should be used as the merge base.
+        parent: Option<CommitSha>,
+    },
     /// Cancel a try build.
     TryCancel,
 }
