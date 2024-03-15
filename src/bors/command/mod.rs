8decd3a701497a4e7e37b570a23da49058a39547
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-03-15 06:44:07 +0000

    docs: document parent options

--- src/bors/command/mod.rs
+++ src/bors/command/mod.rs
@@ -2,9 +2,12 @@ mod parser;
 use crate::github::CommitSha;
 pub use parser::{CommandParseError, CommandParser};
 
+/// Type of parent allowed in a try build
 #[derive(Clone, Debug, PartialEq)]
 pub enum Parent {
+    /// Regular commit sha: parent="<sha>"
     CommitSha(CommitSha),
+    /// Use last build's parent: parent="last"
     Last,
 }
 
