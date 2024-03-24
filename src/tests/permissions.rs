f6b4482aed54b65cf24f23f8f1e243802cb451ce
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-03-24 13:05:10 +0000

    refactor: reload permission on refresh event

--- src/tests/permissions.rs
+++ src/tests/permissions.rs
@@ -8,6 +8,7 @@ impl PermissionResolver for NoPermissions {
     async fn has_permission(&self, _username: &str, _permission: PermissionType) -> bool {
         false
     }
+    async fn reload(&self) {}
 }
 
 pub struct AllPermissions;
@@ -17,4 +18,5 @@ impl PermissionResolver for AllPermissions {
     async fn has_permission(&self, _username: &str, _permission: PermissionType) -> bool {
         true
     }
+    async fn reload(&self) {}
 }
