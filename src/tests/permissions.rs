f665f3056c34ba562b46caf9428c1e496e3df181
vohoanglong0107 <vo.hoang.long@outlook.com>
2024-03-27 08:41:54 +0000

    test: refresh permission

--- src/tests/permissions.rs
+++ src/tests/permissions.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use crate::permissions::{PermissionResolver, PermissionType};
 use axum::async_trait;
 
@@ -19,4 +21,26 @@ impl PermissionResolver for AllPermissions {
         true
     }
     async fn reload(&self) {}
+}
+
+pub struct MockPermissions {
+    pub num_reload_called: i32,
+}
+
+impl Default for MockPermissions {
+    fn default() -> Self {
+        Self {
+            num_reload_called: 0,
+        }
+    }
+}
+
+#[async_trait]
+impl PermissionResolver for Arc<Mutex<MockPermissions>> {
+    async fn has_permission(&self, _username: &str, _permission: PermissionType) -> bool {
+        false
+    }
+    async fn reload(&self) {
+        self.lock().unwrap().num_reload_called += 1
+    }
 }
