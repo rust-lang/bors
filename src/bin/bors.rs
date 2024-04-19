0c6703430dba635c9d50965b2806ac3992fa3a75
Jakub Beránek <berykubik@gmail.com>
2024-04-19 21:15:24 +0200

    Add a few logs and comments

--- src/bin/bors.rs
+++ src/bin/bors.rs
@@ -41,13 +41,17 @@ struct Opts {
     cmd_prefix: String,
 }
 
-async fn server(state: ServerState) -> anyhow::Result<()> {
+/// Starts a server that receives GitHub webhooks and generates events into a queue
+/// that is then handled by the Bors process.
+async fn webhook_server(state: ServerState) -> anyhow::Result<()> {
     let app = create_app(state);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = tokio::net::TcpListener::bind(addr)
         .await
         .context("Cannot create TCP/IP server socket")?;
+
+    tracing::info!("Listening on 0.0.0.0:{}", listener.local_addr()?.port());
 
     axum::serve(listener, app).await?;
     Ok(())
@@ -69,12 +73,14 @@ fn try_main(opts: Opts) -> anyhow::Result<()> {
         .block_on(initialize_db(&opts.db))
         .context("Cannot initialize database")?;
 
-    let state = runtime.block_on(GithubAppState::load(
-        opts.app_id.into(),
-        opts.private_key.into_bytes().into(),
-    ))?;
+    let state = runtime
+        .block_on(GithubAppState::load(
+            opts.app_id.into(),
+            opts.private_key.into_bytes().into(),
+        ))
+        .context("Cannot load GitHub repository state")?;
     let ctx = BorsContext::new(CommandParser::new(opts.cmd_prefix), Arc::new(db));
-    let (tx, gh_process) = create_bors_process(state, ctx);
+    let (tx, bors_process) = create_bors_process(state, ctx);
 
     let refresh_tx = tx.clone();
     let refresh_process = async move {
@@ -85,12 +91,12 @@ fn try_main(opts: Opts) -> anyhow::Result<()> {
     };
 
     let state = ServerState::new(tx, WebhookSecret::new(opts.webhook_secret));
-    let server_process = server(state);
+    let server_process = webhook_server(state);
 
     let fut = async move {
         tokio::select! {
-            () = gh_process => {
-                tracing::warn!("Github webhook process has ended");
+            () = bors_process => {
+                tracing::warn!("Bors event handling process has ended");
                 Ok(())
             },
             res = refresh_process => {
@@ -98,7 +104,7 @@ fn try_main(opts: Opts) -> anyhow::Result<()> {
                 res
             }
             res = server_process => {
-                tracing::warn!("Server has ended: {res:?}");
+                tracing::warn!("GitHub webhook listener has ended: {res:?}");
                 res
             }
         }
@@ -126,7 +132,7 @@ fn main() {
 
     let opts = Opts::parse();
     if let Err(error) = try_main(opts) {
-        eprintln!("Error: {error:?}");
+        tracing::error!("Error: {error:?}");
         std::process::exit(1);
     }
 }
