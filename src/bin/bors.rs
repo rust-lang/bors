4519cfe634c76a035c39859447bf133422176474
Jakub Beránek <berykubik@gmail.com>
2024-05-29 11:41:47 +0200

    Remove temporary DB clear code

--- src/bin/bors.rs
+++ src/bin/bors.rs
@@ -64,24 +64,6 @@ async fn initialize_db(connection_string: &str) -> anyhow::Result<PgDbClient> {
         .await
         .context("Cannot connect to database")?;
 
-    // Temporary code
-    sqlx::query(
-        r#"
-DO $$ DECLARE
-    r RECORD;
-BEGIN
-    -- if the schema you operate on is not "current", you will want to
-    -- replace current_schema() in query with 'schematodeletetablesfrom'
-    -- *and* update the generate 'DROP...' accordingly.
-    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-    END LOOP;
-END $$;
-"#,
-    )
-    .execute(&db)
-    .await
-    .unwrap();
     sqlx::migrate!()
         .run(&db)
         .await
