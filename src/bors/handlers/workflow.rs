8941c08e0dc3946a510240063ac3f6ea6d8de8d2
Sri Ganesh Thota <sriganeshthota12345@gmail.com>
2024-10-19 17:20:46 +0530

    chore: changed status of WorkflowCompleted event

--- src/bors/handlers/workflow.rs
+++ src/bors/handlers/workflow.rs
@@ -62,7 +62,7 @@ pub(super) async fn handle_workflow_started(
 pub(super) async fn handle_workflow_completed(
     repo: Arc<RepositoryState>,
     db: Arc<PgDbClient>,
-    payload: WorkflowCompleted,
+    mut payload: WorkflowCompleted,
 ) -> anyhow::Result<()> {
     if !is_bors_observed_branch(&payload.branch) {
         return Ok(());
@@ -82,6 +82,7 @@ pub(super) async fn handle_workflow_completed(
     if let Some(running_time) = payload.running_time {
         let running_time_as_duration = chrono::Duration::to_std(&running_time).unwrap_or(Duration::from_secs(0));
         if running_time_as_duration < repo.config.load().min_ci_time {
+            payload.status = WorkflowStatus::Failure;
             tracing::warn!(
             "Workflow running time is less than the minimum CI duration: {:?} < {:?}",
             payload.running_time,
