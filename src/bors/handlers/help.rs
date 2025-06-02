ccafd3208aea340c5aa9cfccdff1b7b2f6ca8458
Jakub Beránek <berykubik@gmail.com>
2025-06-02 10:56:24 +0200

    Make help command output nicer

--- src/bors/handlers/help.rs
+++ src/bors/handlers/help.rs
@@ -1,7 +1,6 @@
 use crate::bors::Comment;
 use crate::bors::RepositoryState;
-use crate::bors::command::{Approver, BorsCommand, RollupMode};
-use crate::database::DelegatedPermission;
+use crate::bors::command::BorsCommand;
 use crate::github::PullRequest;
 use std::sync::Arc;
 
@@ -9,83 +8,71 @@ pub(super) async fn command_help(
     repo: Arc<RepositoryState>,
     pr: &PullRequest,
 ) -> anyhow::Result<()> {
-    let help = [
-        BorsCommand::Approve {
-            approver: Approver::Myself,
-            priority: None,
-            rollup: None,
-        },
-        BorsCommand::Approve {
-            approver: Approver::Specified("".to_string()),
-            priority: None,
-            rollup: None,
-        },
-        BorsCommand::Unapprove,
-        BorsCommand::SetPriority(0),
-        BorsCommand::SetDelegate(DelegatedPermission::Review),
-        BorsCommand::Undelegate,
-        BorsCommand::Try {
-            parent: None,
-            jobs: vec![],
-        },
-        BorsCommand::TryCancel,
-        BorsCommand::SetRollupMode(RollupMode::Always),
-        BorsCommand::Info,
-        BorsCommand::Ping,
-        BorsCommand::Help,
-        BorsCommand::OpenTree,
-        BorsCommand::TreeClosed(0),
-    ]
-    .into_iter()
-    .map(|help| format!("- {}", get_command_help(help)))
-    .collect::<Vec<_>>()
-    .join("\n");
+    let help = format_help();
 
     repo.client
-        .post_comment(pr.number, Comment::new(help))
+        .post_comment(pr.number, Comment::new(help.to_string()))
         .await?;
     Ok(())
 }
 
-fn get_command_help(command: BorsCommand) -> String {
-    // !!! When modifying this match, also update the command list above (in [`command_help`]) !!!
-    let help = match command {
+/// Format the bors command help in Markdown format.
+fn format_help() -> &'static str {
+    // The help is generated manually to have a nicer structure.
+    // We do a no-op destructuring of `BorsCommand` to make it harder to modify help in case new
+    // commands are added though.
+    match BorsCommand::Ping {
         BorsCommand::Approve {
-            approver: Approver::Myself,
-            ..
-        } => {
-            "`r+ [p=<priority>] [rollup=<never|iffy|maybe|always>]`: Approve this PR. Optionally, you can specify `<priority>`, `<rollup>`."
-        }
-        BorsCommand::Approve {
-            approver: Approver::Specified(_),
-            ..
-        } => {
-            "`r=<user> [p=<priority>]`: Approve this PR on behalf of `<user>`. Optionally, you can specify a `<priority>`."
-        }
-        BorsCommand::Unapprove => "`r-`: Unapprove this PR",
-        BorsCommand::SetPriority(_) => "`p=<priority>`: Set the priority of this PR",
-        BorsCommand::SetDelegate(_) => {
-            "`delegate=<try|review>`: Delegate permissions to the PR author\n- `delegate+`: Delegate review permissions to the PR author"
-        }
-        BorsCommand::Undelegate => "`delegate-`: Remove any previously granted delegation",
-        BorsCommand::Help => "`help`: Print this help message",
-        BorsCommand::Ping => "`ping`: Check if the bot is alive",
-        BorsCommand::Try { .. } => {
-            "`try [parent=<parent>] [jobs=<jobs>]`: Start a try build. Optionally, you can specify a `<parent>` SHA or a list of `<jobs>` to run"
-        }
-        BorsCommand::TryCancel => "`try cancel`: Cancel a running try build",
-        BorsCommand::SetRollupMode(_) => {
-            "`rollup=<never|iffy|maybe|always>`: Mark the rollup status of the PR"
-        }
-        BorsCommand::Info => {
-            "`info`: Get information about the current PR including delegation, priority, merge status, and try build status"
-        }
-        BorsCommand::OpenTree => "`treeclosed-`, `treeopen`: Open the repository tree for merging",
-        BorsCommand::TreeClosed(_) => {
-            "`treeclosed=<priority>`: Close the tree for PRs with priority less than `<priority>`"
-        }
-    };
-    help.to_string()
+            approver: _,
+            rollup: _,
+            priority: _,
+        } => {}
+        BorsCommand::Unapprove => {}
+        BorsCommand::Help => {}
+        BorsCommand::Ping => {}
+        BorsCommand::Try { parent: _, jobs: _ } => {}
+        BorsCommand::TryCancel => {}
+        BorsCommand::SetPriority(_) => {}
+        BorsCommand::Info => {}
+        BorsCommand::SetDelegate(_) => {}
+        BorsCommand::Undelegate => {}
+        BorsCommand::SetRollupMode(_) => {}
+        BorsCommand::OpenTree => {}
+        BorsCommand::TreeClosed(_) => {}
+    }
+
+    r#"
+You can use the following commands:
+
+## PR management
+- `r+ [p=<priority>] [rollup=<never|iffy|maybe|always>]`: Approve this PR on your behalf
+    - Optionally, you can specify the `<priority>` of the PR and if it is eligible for rollups (`<rollup>)`.
+- `r=<user> [p=<priority>] [rollup=<never|iffy|maybe|always>]`: Approve this PR on behalf of `<user>`
+    - Optionally, you can specify the `<priority>` of the PR and if it is eligible for rollups (`<rollup>)`.
+    - You can pass a comma-separated list of GitHub usernames.
+- `r-`: Unapprove this PR
+- `p=<priority>` or `priority=<priority>`: Set the priority of this PR
+- `rollup=<never|iffy|maybe|always>`: Set the rollup status of the PR
+- `delegate=<try|review>`: Delegate permissions for running try builds or approving to the PR author
+    - `try` allows the PR author to start try builds.
+    - `review` allows the PR author to both start try builds and approve the PR.
+- `delegate+`: Delegate approval permissions to the PR author
+    - Shortcut for `delegate=review`
+- `delegate-`: Remove any previously granted permission delegation
+- `try [parent=<parent>] [jobs=<jobs>]`: Start a try build.
+    - Optionally, you can specify a `<parent>` SHA with which will the PR be merged. You can specify `parent=last` to use the same parent SHA as the previous try build.
+    - Optionally, you can select a comma-separated list of CI `<jobs>` to run in the try build.
+- `try cancel`: Cancel a running try build
+- `info`: Get information about the current PR
+
+## Repository management
+- `treeclosed=<priority>`: Close the tree for PRs with priority less than `<priority>`
+- `treeclosed-` or `treeopen`: Open the repository tree for merging
+
+## Meta commands
+- `ping`: Check if the bot is alive
+- `help`: Print this help message
+"#
 }
 
 #[cfg(test)]
@@ -97,21 +84,36 @@ mod tests {
         run_test(pool, |mut tester| async {
             tester.post_comment("@bors help").await?;
             insta::assert_snapshot!(tester.get_comment().await?, @r"
-            - `r+ [p=<priority>] [rollup=<never|iffy|maybe|always>]`: Approve this PR. Optionally, you can specify `<priority>`, `<rollup>`.
-            - `r=<user> [p=<priority>]`: Approve this PR on behalf of `<user>`. Optionally, you can specify a `<priority>`.
+            You can use the following commands:
+
+            ## PR management
+            - `r+ [p=<priority>] [rollup=<never|iffy|maybe|always>]`: Approve this PR on your behalf
+                - Optionally, you can specify the `<priority>` of the PR and if it is eligible for rollups (`<rollup>)`.
+            - `r=<user> [p=<priority>] [rollup=<never|iffy|maybe|always>]`: Approve this PR on behalf of `<user>`
+                - Optionally, you can specify the `<priority>` of the PR and if it is eligible for rollups (`<rollup>)`.
+                - You can pass a comma-separated list of GitHub usernames.
             - `r-`: Unapprove this PR
-            - `p=<priority>`: Set the priority of this PR
-            - `delegate=<try|review>`: Delegate permissions to the PR author
-            - `delegate+`: Delegate review permissions to the PR author
-            - `delegate-`: Remove any previously granted delegation
-            - `try [parent=<parent>] [jobs=<jobs>]`: Start a try build. Optionally, you can specify a `<parent>` SHA or a list of `<jobs>` to run
+            - `p=<priority>` or `priority=<priority>`: Set the priority of this PR
+            - `rollup=<never|iffy|maybe|always>`: Set the rollup status of the PR
+            - `delegate=<try|review>`: Delegate permissions for running try builds or approving to the PR author
+                - `try` allows the PR author to start try builds.
+                - `review` allows the PR author to both start try builds and approve the PR.
+            - `delegate+`: Delegate approval permissions to the PR author
+                - Shortcut for `delegate=review`
+            - `delegate-`: Remove any previously granted permission delegation
+            - `try [parent=<parent>] [jobs=<jobs>]`: Start a try build.
+                - Optionally, you can specify a `<parent>` SHA with which will the PR be merged. You can specify `parent=last` to use the same parent SHA as the previous try build.
+                - Optionally, you can select a comma-separated list of CI `<jobs>` to run in the try build.
             - `try cancel`: Cancel a running try build
-            - `rollup=<never|iffy|maybe|always>`: Mark the rollup status of the PR
-            - `info`: Get information about the current PR including delegation, priority, merge status, and try build status
+            - `info`: Get information about the current PR
+
+            ## Repository management
+            - `treeclosed=<priority>`: Close the tree for PRs with priority less than `<priority>`
+            - `treeclosed-` or `treeopen`: Open the repository tree for merging
+
+            ## Meta commands
             - `ping`: Check if the bot is alive
             - `help`: Print this help message
-            - `treeclosed-`, `treeopen`: Open the repository tree for merging
-            - `treeclosed=<priority>`: Close the tree for PRs with priority less than `<priority>`
             ");
             Ok(tester)
         })
