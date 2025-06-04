fe4c724e7aaa417595957d86768d7e8e737f245e
Jakub Beránek <berykubik@gmail.com>
2025-06-04 08:07:50 +0200

    Move some comments to the `comment` module

--- src/bors/comment.rs
+++ src/bors/comment.rs
@@ -96,6 +96,53 @@ pub fn workflow_failed_comment(workflows: &[WorkflowModel]) -> Comment {
     ))
 }
 
+pub fn try_build_started_comment(
+    head_sha: &CommitSha,
+    merge_sha: &CommitSha,
+    bot_prefix: &str,
+) -> Comment {
+    Comment::new(format!(
+        ":hourglass: Trying commit {head_sha} with merge {merge_sha}…
+
+To cancel the try build, run the command `{bot_prefix} try cancel`."
+    ))
+}
+
+pub fn merge_conflict_comment(branch: &str) -> Comment {
+    let message = format!(
+        r#":lock: Merge conflict
+
+This pull request and the master branch diverged in a way that cannot
+ be automatically merged. Please rebase on top of the latest master
+ branch, and let the reviewer approve again.
+
+<details><summary>How do I rebase?</summary>
+
+Assuming `self` is your fork and `upstream` is this repository,
+ you can resolve the conflict following these steps:
+
+1. `git checkout {branch}` *(switch to your branch)*
+2. `git fetch upstream master` *(retrieve the latest master)*
+3. `git rebase upstream/master -p` *(rebase on top of it)*
+4. Follow the on-screen instruction to resolve conflicts (check `git status` if you got lost).
+5. `git push self {branch} --force-with-lease` *(update this PR)*
+
+You may also read
+ [*Git Rebasing to Resolve Conflicts* by Drew Blessing](http://blessing.io/git/git-rebase/open-source/2015/08/23/git-rebasing-to-resolve-conflicts.html)
+ for a short tutorial.
+
+Please avoid the ["**Resolve conflicts**" button](https://help.github.com/articles/resolving-a-merge-conflict-on-github/) on GitHub.
+ It uses `git merge` instead of `git rebase` which makes the PR commit history more difficult to read.
+
+Sometimes step 4 will complete without asking for resolution. This is usually due to difference between how `Cargo.lock` conflict is
+handled during merge and rebase. This is normal, and you should still perform step 5 to update this PR.
+
+</details>
+"#
+    );
+    Comment::new(message)
+}
+
 fn list_workflows_status(workflows: &[WorkflowModel]) -> String {
     workflows
         .iter()
