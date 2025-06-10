5b5eccc7a685cc4eb65606d1e9712bc0c78ca298
Jakub Beránek <berykubik@gmail.com>
2025-06-10 11:39:36 +0200

    Document `TestSyncMarker` and remove unused field

--- src/tests/util.rs
+++ src/tests/util.rs
@@ -1,7 +1,10 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
 use thread_local::ThreadLocal;
 use tokio::sync;
 
+/// This struct serves for waiting for certain async (and unsynchronized) events to happen in tests.
+/// The test should start an async operation and then call `sync().await` to wait until it's
+/// finished. The expectation is that the operation will eventually call `mark()` to unblock the
+/// test.
 pub struct TestSyncMarker {
     inner: ThreadLocal<TestSyncMarkerInner>,
 }
@@ -30,7 +33,6 @@ impl TestSyncMarker {
 struct TestSyncMarkerInner {
     rx: sync::Mutex<sync::mpsc::Receiver<()>>,
     tx: sync::mpsc::Sender<()>,
-    hits: AtomicUsize,
 }
 
 impl TestSyncMarkerInner {
@@ -39,7 +41,6 @@ impl TestSyncMarkerInner {
         Self {
             tx,
             rx: sync::Mutex::new(rx),
-            hits: AtomicUsize::new(0),
         }
     }
 
@@ -47,7 +48,6 @@ impl TestSyncMarkerInner {
     pub fn mark(&self) {
         // If we cannot send, don't block the program.
         let _ = self.tx.try_send(());
-        self.hits.fetch_add(1, Ordering::SeqCst);
     }
 
     /// Wait until code has encountered this location.
