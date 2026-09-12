eb93aaeba325492ca462d81aa394d75034d3a572
David Manca <1900179+medzernik@users.noreply.github.com>
2026-09-12 14:07:23 +0200

    `SquashAndApprove` -> `SquashApprove`
    Signed-off-by: David Manca <1900179+medzernik@users.noreply.github.com>

--- src/bors/handlers/mod.rs
+++ src/bors/handlers/mod.rs
@@ -703,7 +703,7 @@ async fn handle_comment(
                             Ok(())
                         }
                     }
-                    BorsCommand::SquashAndApprove {
+                    BorsCommand::SquashApprove {
                         commit_message,
                         approver,
                         priority,
--- src/bors/mod.rs
+++ src/bors/mod.rs
@@ -109,7 +109,7 @@ pub fn format_help() -> &'static str {
         BorsCommand::Retry => {}
         BorsCommand::Cancel => {}
         BorsCommand::Squash { .. } => {}
-        BorsCommand::SquashAndApprove { .. } => {}
+        BorsCommand::SquashApprove { .. } => {}
     }
 
     r#"
