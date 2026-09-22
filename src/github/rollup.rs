dceae07700f1a82bdf1f0917b3511df616505f9f
Jakub Beránek <berykubik@gmail.com>
2026-09-22 09:44:29 +0200

    Do not link to GitHub PRs when doing rollup mergeability check

--- src/github/rollup.rs
+++ src/github/rollup.rs
@@ -505,7 +505,7 @@ async fn has_pending_auto_build_conflict(
     pending_auto_pr_number: PullRequestNumber,
 ) -> Result<bool, RollupError> {
     let merge_message =
-        format!("Rollup compatibility check against pending auto build #{pending_auto_pr_number}");
+        format!("Rollup compatibility check against pending auto build {pending_auto_pr_number}");
     // Merge the pending auto build's commit into the rollup branch
     let has_conflict = match client
         .merge_branches(
